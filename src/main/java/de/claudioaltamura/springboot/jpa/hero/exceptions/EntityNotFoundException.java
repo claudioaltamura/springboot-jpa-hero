68b2b7e3115300ee8087bd3e232dc1efe6273456
claudioaltamura <claudio@claudioaltamura.de>
2020-07-05 10:51:53 +0200

    chg: added id

--- src/main/java/de/claudioaltamura/springboot/jpa/hero/exceptions/EntityNotFoundException.java
+++ src/main/java/de/claudioaltamura/springboot/jpa/hero/exceptions/EntityNotFoundException.java
@@ -2,4 +2,12 @@ package de.claudioaltamura.springboot.jpa.hero.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
 
+    private final long id;
+
+    public EntityNotFoundException(long id) {
+        super("entity not found '" + id + "'.");
+        this.id = id;
+    }
+
+
 }
